22837a8fe72b1b082cbdb1ee8ff66edb88102369
Long Nguyen Hoang <hoanglongdev533@gmail.com>
2024-10-12 05:41:15 +0700

    user can comment into the room

--- src/components/RoomFeedbacks.vue
+++ src/components/RoomFeedbacks.vue
@@ -1,18 +1,26 @@
 <template>
-  <a-list :grid="{ gutter: 18, column: 2 }" :pagination="pagination" :data-source="feedbacksInfo">
-    <template #renderItem="{ item }">
-      <Feedback
-        :authorFullName="item.authorFullName"
-        :description="item.description"
-        :createdDate="item.createdDate"
-      />
-    </template>
-  </a-list>
+  <div class="flex flex-col gap-4">
+    <a-input-group compact>
+      <a-input v-model:value="commentInput" style="width: calc(70% - 200px)" />
+      <a-button type="primary" @click="onComment">Submit</a-button>
+    </a-input-group>
+    <a-list :grid="{ gutter: 18, column: 2 }" :pagination="pagination" :data-source="feedbacksInfo">
+      <template #renderItem="{ item }">
+        <Feedback
+          :authorFullName="item.authorFullName"
+          :description="item.description"
+          :createdDate="item.createdDate"
+        />
+      </template>
+    </a-list>
+  </div>
 </template>
 <script lang="ts" setup>
 import { computed, ref, watch } from 'vue'
 import Feedback, { type FeedbackProps } from './Feedback.vue'
 import axios from 'axios'
+import { useUser } from 'vue-clerk'
+import { message } from 'ant-design-vue'
 
 interface RoomFeedbacksProps {
   roomId: string
@@ -22,6 +30,8 @@ const roomFeedbacksData = defineProps<RoomFeedbacksProps>()
 const feedbacksInfo = ref<FeedbackProps[]>()
 const totalFeedbacksCount = ref<number>()
 const currentPage = ref<number>(1)
+const commentInput = ref<string>('')
+const { user } = useUser()
 
 const pagination = computed(() => ({
   onChange: (page: number) => {
@@ -44,6 +54,22 @@ const loadFeedbacks = async (page: number) => {
   }))
 
   totalFeedbacksCount.value = res.data.total
+}
+
+const onComment = async () => {
+  const commentInfo = {
+    roomId: roomFeedbacksData.roomId,
+    clerkId: user.value?.id,
+    rating: 5,
+    comment: commentInput.value
+  }
+
+  const res = await axios.post('http://localhost:8000/api/room-feedbacks/', commentInfo)
+  if (res.status === 201) {
+    message.success('Add feedback successfully!')
+    commentInput.value = ''
+    loadFeedbacks(1)
+  }
 }
 
 watch(
